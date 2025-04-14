b2921c8a37bc44106557ba8ac4168488bfb58da5
eric <eric940724@gmail.com>
2025-04-14 11:28:47 +0800

    歷史紀錄bug

--- js/ranking.js
+++ js/ranking.js
@@ -17,18 +17,27 @@ function addfirebase(s){
     namelist.push(currentURLname);
     namelist.push("score:"+s);
     */
+   var s_now
    var isNoSamName = true;
    if(namelist.includes(playerName)){
-        for(var i=0;i<namelist.length;i++){
+        for(var i=1;i<namelist.length;i++){
             if(namelist[i]==playerName){
                 if(GameNamelist[i]==currentURLname){
-                    if(parseInt(scorelist[i].split(":").pop()) >= s) isNoSamName = false;
+                    s_now =  parseInt(scorelist[i].split(":").pop());
+                    if(s_now >= s) isNoSamName = false;
+                    
                 }
             }
         }
    }
+   //window.alert("daf") +parseInt(scorelist[i].split(":").pop())+"\n"
+   /*
+   for(var i=1;i<namelist.length;i++){
+        s_now = parseInt(scorelist[i].split(":").pop());
+        window.alert(currentURLname+"\n"+playerName+"\n"+s_now+"\n"+s)
+   }*/
    if(isNoSamName){
-       namelist.push(playerName);
+       namelist.push(playerName);scorelist
        GameNamelist.push(currentURLname);
        scorelist.push("score:"+s);
    
@@ -56,32 +65,32 @@ function showranking(){
                 if (snapshot.exists()) scorelist = snapshot.val();//list
                 rankinglist = [];
                 let x = 1;
-                for(var i=0;i<GameNamelist.length;i++){
+                for(var i=1;i<GameNamelist.length;i++){
                     if(GameNamelist[i] == currentURLname){
                         rankinglist.push(namelist[i]);
                         rankinglist.push(scorelist[i]);
-                        //window.alert(namelist[0]);
                     }
                 }
+                //window.alert(rankinglist);
                 var maxidlist = [],textUItext = "";
                 for(var i=1;i < (rankinglist.length );i+=2){
-                    var maxid ; 
+                    var maxid = null; 
                     var max = 0
                     for(var j=1;j < rankinglist.length;j+=2){
-                        //window.alert(j+"\n"+maxidlist)
                         if(!maxidlist.includes(j)){
+                            //window.alert(j+"\n"+maxidlist+"\n"+!maxidlist.includes(j))
                             var s1 =  parseInt(rankinglist[j].split(":").pop())
                             //window.alert(s1);
                             if(s1 > max || maxid == null){
-                                //window.alert(rankinglist[j])
                                 //window.alert(j+"\n"+maxidlist)
                                 maxid = j ;
                                 max =  parseInt(rankinglist[maxid].split(":").pop())
+                                //window.alert(max)
                             }
                         }
                     }
-                    //window.alert(maxid);
                     maxidlist.push(maxid);
+                    //window.alert(maxidlist);
                     textUItext += rankinglist[maxid-1] + " " + rankinglist[maxid] + "<br>";
                 }
             p_textUI.innerHTML = textUItext;
